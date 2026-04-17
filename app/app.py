2cf011078ea79f9fce77a96fa3cf240577cdccd4
Jacky <jackykukadiya@gmail.com>
2026-04-17 17:22:03 +0530

    path issue

--- app/app.py
+++ app/app.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 import joblib
+import os
 
 app = FastAPI()
 
-model = joblib.load("models/model.pkl")
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # project root
+model_path = os.path.join(BASE_DIR, "model", "model.pkl")
+
+print("Model path:", model_path)
+print("Exists:", os.path.exists(model_path))
+
+try:
+    model = joblib.load(model_path)
+    print("Model loaded successfully")
+except Exception as e:
+    print("Error loading model:", e)
+    model = None
+    
 @app.get("/")
 def home():
     return {"status": "Model is running"}
